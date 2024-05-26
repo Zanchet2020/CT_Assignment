d4e30df5cdff7e421aaaff6b3c363ec1a84838e5
Lucas Samuel Zanchet <lucasamuel040@gmail.com>
2024-05-26 11:15:46 -0300

    working charachter stack

--- main.c
+++ main.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STACK_INIT_SIZE 128
+#define STACK_INIT_SIZE 32
 
 typedef struct {
-  Datatype type_size;
   char * data;
   size_t capacity;
   size_t count;
 } Stack;
 
-Stack * new_stack(Datatype type){
+Stack * new_stack(){
   Stack * new = malloc(sizeof(Stack));
-  new->type =(size_t)type;
   new->capacity = 0;
   new->count = 0;
   new->data = NULL;
   return new;
 }
 
-void push_stack(Stack * s, void * value){
+void push_stack(Stack * s, char value){
   if(s->count >= s->capacity){
     s->capacity += STACK_INIT_SIZE;
-    s->data = realloc(s->data, s->capacity * s->type_size);
+    s->data = realloc(s->data, s->capacity * sizeof(s->data[0]));
   }
-  *((uint8_t**)s->data * s->count * s->type_size) = value;
-  s->count++;
+  s->data[s->count++]= value;
 }
 
-void * pop_stack(Stack * s){
-  return ((uint_t**)s->data)[s->count--];
+char pop_stack(Stack * s){
+  return s->data[--s->count];
+}
+
+int is_empty_stack(Stack * s){
+  return !s->count;
+}
+
+void free_stack(Stack * s){
+  free(s->data);
+  free(s);
 }
 
 int main(){
-  printf("Hello World");  
+  Stack* s = new_stack();
+  for(int i=0;i<200;i++)
+    push_stack(s, (char)i);
+  
+  while(!is_empty_stack(s)) printf("%c\n", pop_stack(s));
+
+  free_stack(s);
   return 0;
 }
