c9c513e2711d58f05d013ce74bb7df075d12f8c7
Zanchet2020 <lucasamuel040@gmail.com>
2024-05-30 13:51:45 -0300

    working pda

--- main.c
+++ main.c
@@ -1,59 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define STACK_INIT_SIZE 32
+#include "stack.h"
+
+#define STRING_INIT_SIZE 10
 
 typedef struct {
+  char * text;
+  size_t count;
+  size_t capacity;
+} String;
 
-} Node;
+String * new_string(){
+  String * str = (String*)malloc(sizeof(String));
+  str->count = 0;
+  str->capacity = STRING_INIT_SIZE;
+  str->text = (char*) malloc(str->capacity * sizeof(char));
+  return str;
+}
+
+void append_to_string(String* str, char * txt){
+  for(size_t i = 0; txt[i] != '\0'; ++i){
+    if(str->count >= str->capacity){
+      str->capacity += STRING_INIT_SIZE;
+      str->text = realloc(str->text, str->capacity * sizeof(char));
+    }
+    str->text[i + str->count] = txt[i];
+    str->count++;
+  }
+}
+
+void free_string(String * str){
+  free(str->text);
+  free(str);
+}
+
 
 typedef struct {
-  Node * nodes;
-  
+  char consume;
+  char unstack;
+  String * stack;
+} Transition;
+
+typedef struct {
+  Transition ** transitions;
+  bool * is_final_state;
 } Graph;
 
 typedef struct {
-  char * data;
-  size_t capacity;
-  size_t count;
-} Stack;
+  Stack * stack;
+  Graph states;
+  size_t num_of_states;
+  
+} PDA;
 
-Stack * new_stack(){
-  Stack * new = malloc(sizeof(Stack));
-  new->capacity = 0;
-  new->count = 0;
-  new->data = NULL;
-  return new;
-}
-
-void push_stack(Stack * s, char value){
-  if(s->count >= s->capacity){
-    s->capacity += STACK_INIT_SIZE;
-    s->data = realloc(s->data, s->capacity * sizeof(s->data[0]));
+PDA * new_pda(size_t number_of_states){
+  size_t Q = number_of_states;
+  PDA * pda = (PDA*)malloc(sizeof(*pda));
+  pda->num_of_states = Q;
+  pda->stack = new_stack();
+  pda->states.transitions = (Transition**) malloc(Q*sizeof(Transition*));
+  pda->states.is_final_state = (bool*) malloc(Q*sizeof(bool));
+  for(size_t i = 0; i < Q; ++i){
+    pda->states.transitions[i] = (Transition*) malloc(Q*sizeof(Transition));
+    pda->states.is_final_state[i] = false;
+    for(size_t j = 0; j < Q; ++j){
+      pda->states.transitions[i][j].stack = new_string();
+    }
   }
-  s->data[s->count++]= value;
+  return pda;
 }
 
-char pop_stack(Stack * s){
-  return s->data[--s->count];
-}
-
-int is_empty_stack(Stack * s){
-  return !s->count;
-}
-
-void free_stack(Stack * s){
-  free(s->data);
-  free(s);
+void free_pda(PDA * pda){
+  for(size_t i = 0; i < pda->num_of_states; ++i){
+    for(size_t j = 0; j < pda->num_of_states; ++j){
+      free_string(pda->states.transitions[i][j].stack);
+    }
+    free(pda->states.transitions[i]);
+  }
+  free(pda->states.transitions);
+  free_stack(pda->stack);
+  free(pda);
 }
 
 int main(){
-  Stack* s = new_stack();
-  for(int i=0;i<200;i++)
-    push_stack(s, (char)i);
+  size_t Q = 0, T = 0;
+  scanf("%zu %zu", &Q, &T);
+  PDA * pda = new_pda(Q);
   
-  while(!is_empty_stack(s)) printf("%c\n", pop_stack(s));
+  for(size_t i = 0; i < T; ++i){
+    size_t from_state, to_state;
+    char consume, unstack;
+    char stack[100];
+    scanf("%zu %c %c %s %zu", &from_state, &consume, &unstack, stack, &to_state);
+    //    printf("%zu %c %c %s %zu", from_state, consume, unstack, stack, to_state);
+    Transition t = {
+      .consume = consume,
+      .unstack = unstack,
+      .stack = new_string()
+    };
+    append_to_string(t.stack, stack);
+    pda->states.transitions[from_state][to_state] = t;
+  }
 
-  free_stack(s);
+  size_t num_of_final_states = 0;
+  scanf("%zu", &num_of_final_states);
+  for(size_t i = 0; i < num_of_final_states; ++i){
+    size_t index;
+    scanf("%zu", &index);
+    pda->states.is_final_state[index] = true;
+  }
+  
+  free_pda(pda);
+  //  printf("Hello\n");
   return 0;
 }
---
+++ stack.h
@@ -0,0 +1,36 @@
+#define STACK_INIT_SIZE 16
+
+typedef struct {
+  char * data;
+  size_t capacity;
+  size_t count;
+} Stack;
+
+Stack * new_stack(){
+  Stack * new = malloc(sizeof(Stack));
+  new->capacity = 0;
+  new->count = 0;
+  new->data = NULL;
+  return new;
+}
+
+void push_stack(Stack * s, char value){
+  if(s->count >= s->capacity){
+    s->capacity += STACK_INIT_SIZE;
+    s->data = realloc(s->data, s->capacity * sizeof(s->data[0]));
+  }
+  s->data[s->count++]= value;
+}
+
+char pop_stack(Stack * s){
+  return s->data[--s->count];
+}
+
+int is_empty_stack(Stack * s){
+  return !s->count;
+}
+
+void free_stack(Stack * s){
+  free(s->data);
+  free(s);
+}
